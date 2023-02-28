bf63686ba021186de0c31e888eac76d3176d0835
Felipe Carvalho <104037790+LippeTK@users.noreply.github.com>
2023-02-28 12:31:06 -0300

    Update faturamentos.js

--- faturamentos.js
+++ faturamentos.js
@@ -1,34 +1,35 @@
-// Acredito que eu deveria ter recebido por e-mail, porém não recebi nenhum arquivo json ou xml para fonte de dados, então gerei números aleatórios e os coloquei na variável abaixo
-const faturamentoDiario = [900, 1100, 800, 1200, 0, 0, 1500, 1200, 1300, 1000, 800, 1100, 1200, 900, 1300, 1400, 1000, 1100, 900, 800, 1200, 1400, 1300, 1100, 1000, 1200, 800, 900, 1500, 1400, 1300];
+const fs = require('fs');
 
-let menorFaturamento = faturamentoDiario[0];
-let maiorFaturamento = faturamentoDiario[0];
+const jsonFile = fs.readFileSync('./faturamento.json');
+const faturamentoDiario = JSON.parse(jsonFile);
+
+let menorFaturamento = faturamentoDiario[0].valor;
+let maiorFaturamento = faturamentoDiario[0].valor;
 let somaFaturamento = 0;
 let diasAcimaDaMedia = 0;
 let diasComFaturamento = 0;
 
 for (let i = 0; i < faturamentoDiario.length; i++) {
-
-  if (faturamentoDiario[i] < menorFaturamento) {
-    menorFaturamento = faturamentoDiario[i];
-  }if (faturamentoDiario[i] > maiorFaturamento) {
-    maiorFaturamento = faturamentoDiario[i];
+  if (faturamentoDiario[i].valor < menorFaturamento) {
+    menorFaturamento = faturamentoDiario[i].valor;
   }
-  somaFaturamento += faturamentoDiario[i];
+  if (faturamentoDiario[i].valor > maiorFaturamento) {
+    maiorFaturamento = faturamentoDiario[i].valor;
+  }
+  somaFaturamento += faturamentoDiario[i].valor;
 
-  if (faturamentoDiario[i] > 0) { //não conta os dias com 0 como dias de faturamento
+  if (faturamentoDiario[i].valor > 0) { //não conta os dias com 0 como dias de faturamento
     diasComFaturamento++;
   }
 }
 
 const mediaMensal = somaFaturamento / diasComFaturamento;
 for (let i = 0; i < faturamentoDiario.length; i++) {
-    if (faturamentoDiario[i] > mediaMensal) {
-      diasAcimaDaMedia++;
-    }
+  if (faturamentoDiario[i].valor > mediaMensal) {
+    diasAcimaDaMedia++;
+  }
 }
 
-// imprime os resultados na tela
 console.log("Menor valor de faturamento ocorrido em um dia do mês: " + menorFaturamento);
 console.log("Maior valor de faturamento ocorrido em um dia do mês: " + maiorFaturamento);
 console.log("Dias com faturamento acima da média: " + diasAcimaDaMedia);
